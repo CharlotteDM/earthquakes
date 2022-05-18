e220542d02a2b2c5ccd71ffdb3f67e40936c3c7f
CharlotteDM <carolinemularczyk@gmail.com>
2022-05-18 22:13:30 +0200

    Update analysis-earthquakes.R

--- analysis-earthquakes.R
+++ analysis-earthquakes.R
@@ -19,36 +19,6 @@ ertqk <- read.csv("data/earthquakes.csv", stringsAsFactors = F)
 #data source: https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_month.csv
 
 
-#sets color
-pal_ertqk <- colorFactor(palette = "Set3", domain = ertqk[["mag"]])
-
-#creates map
-ertqk_map <- leaflet(data = ertqk) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addCircleMarkers(
-    lat = ~latitude,
-    lng = ~longitude,
-    label = ~paste0("</br> Depth of the event in kilometers: ", depth,
-                    "</br> MAgnitude: ", mag),
-    color = ~pal_ertqk(ertqk[["mag"]]),
-    fillOpacity = .7,
-    radius = 4,
-    stroke = F) %>%
-  addLegend(
-    position = "bottomright",
-    title = "Eartquakes",
-    pal = pal_ertqk,
-    values = ~mag,
-    opacity = .5)
-
-
-#number of physicians in a given specialization
-
-ertqk_table <- ertqk %>%
-  group_by(magType) %>%
-  arrange(-mag) %>%
-  select(time, depth, mag, magType, gap, dmin, rms)
-
 #application interface
 my_ui_earthquake <- fluidPage(
   titlePanel("M4.5+ Earthquakes in the world (18.04.2022 - 18.05.2022)"),
@@ -56,26 +26,41 @@ my_ui_earthquake <- fluidPage(
     sidebarPanel(
       selectInput(
         inputId = "analysis_var",
-        label = "Magnitude:",
-        choices = unique(ertqk$mag))),
-        mainPanel(
-          leafletOutput(outputId = "ertqk_map"),
-          leafletOutput(outputId = "ertqk_table"))))
+        label = "Analysis level",
+        choices = c("mag", "magType")
+        )
+      ),
+    mainPanel(
+      leafletOutput("ertqk_map"),
+      tableOutput("ertqk_table")
+      )
+    )
+  )
+
+
+
+#number of earhqk
+
+#ertqk_table <- ertqk %>%
+#  group_by(magType) %>%
+#  arrange(-mag) %>%
+#  select(time, depth, mag, magType, gap, dmin, rms)
 
 
 
 
 #run app
-server <- function(input, output, sessions) {}
-shinyApp(ui = my_ui_earthquake, server = server)  
+#server <- function(input, output, sessions) {}
+#shinyApp(ui = my_ui_earthquake, server = server)  
 
 
 
 #refine code
-my_server <- function(input, output, session) {
+my_server <- function(input, output) {
+  
   output$ertqk_map <- renderLeaflet({
     pal_ertqk <- colorFactor(
-      palette = "Set3",
+      palette = "Dark2",
       domain = ertqk[[input$analysis_var]]
     )
     leaflet(data = ertqk) %>%
@@ -83,19 +68,26 @@ my_server <- function(input, output, session) {
       addCircleMarkers(
         lat = ~latitude,
         lng = ~longitude,
-        label = ~paste0("</br> Depth of the event in kilometers: ", depth,
-                        "</br> MAgnitude: ", mag),
-        color = ~pal_ertqk(ertqk[["mag"]]),
+        label = ~paste("Depth of the event in kilometers: ", depth,
+                        "Magnitude: ", mag),
+        color = ~pal_ertqk(ertqk[[input$analysis_var]]),
         fillOpacity = .7,
         radius = 4,
         stroke = F) %>%
       addLegend(
         position = "bottomright",
-        title = "Eartquakes",
+        title = "Magnitude Type",
         pal = pal_ertqk,
-        values = ~mag,
+        values = ~ertqk[[input$analysis_var]],
         opacity = .5)
  })
+  output$grouped_table <- renderTable({
+    ertqk_table <- ertqk %>%
+      group_by(ertqk[[input$analysis_var]]) %>%
+      arrange(-input$analysis_var) 
+      colnames(ertqk_table) <- c(input$analysis_var, "Magnitude")
+      
+  })
 }
 
 
