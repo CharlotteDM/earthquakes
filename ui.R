80d5a6d8ecc774240820e151ccd093c8e5e1a5a9
CharlotteDM <carolinemularczyk@gmail.com>
2024-01-08 18:07:45 +0100

    hash path & some packages; new deployment

--- ui.R
+++ ui.R
@@ -4,24 +4,21 @@ library(shiny)
 library(RColorBrewer)
 library(rstudioapi)
 library(leaflet)
-library(htmlwidgets)
+#library(htmlwidgets)
 library(ggplot2)
-library(jsonlite)
+
 
 #install.packages("rsconnect")
 #install.packages("shinyWidgets")
 #install.packages("shinythemes")
 
 library(rsconnect)
-library(shinyWidgets)
+#library(shinyWidgets)
 library(shinythemes)
 
 #uncomment to set working directory of RStudio - only for local
-path <- dirname(rstudioapi::getActiveDocumentContext()$path)
-setwd(path)
-
-erthqs <- fromJSON("https://earthquake.usgs.gov/fdsnws/event/1/application.json")
-
+#path <- dirname(rstudioapi::getActiveDocumentContext()$path)
+#setwd(path)
 
 
 ertqk <- read.csv("data/earthquakes.csv", stringsAsFactors = F)
